32b204741012fe83a81bc72435102524d75ec54f
yuvi164 <yuvrajrana1604@gmail.com>
2019-01-18 14:08:12 +0530

    Updated style

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,24 @@ end
 =end
 
 #CHANGE ROUHTS # TO
-
+=begin
 Rails.application.routes.draw do
 resources :todo_lists do
    resources :todo_items
   end
+root "todo_lists#index"
+end
+=end
+
+#after adding new colmn in data base name add_completed_at_to_todo_items update route file to
+
+Rails.application.routes.draw do
+resources :todo_lists do
+   resources :todo_items do
+    member do
+     patch :complete
+    end
+  end
+end
 root "todo_lists#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_120341) do
+ActiveRecord::Schema.define(version: 2019_01_18_064303) do
 
   create_table "todo1_lists", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_01_17_120341) do
     t.integer "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
